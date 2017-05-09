753eb9ae0da9ec57494a58bfcada5c92f1245766
Vishal Lama <vishal.lama@gmail.com>
2017-05-09 13:55:47 -0600

    Add typing rule for dependent case analysis

--- src/CaseAnalysisAndPatternMatching.v
+++ src/CaseAnalysisAndPatternMatching.v
@@ -24,3 +24,22 @@
       match O return B with O => t0 | S p => ts end ==> to
 
       match S q return B with O => t0 | S p => ts end ==> ts{q/p} *)
+
+
+(* 3.2 Dependent Case Analysis *)
+
+(* For a pattern matching construct of the form 'match n with ... end', a
+   more general typing rule is obtained considering that the type of the
+   whole expression may also depend on n. For instance, consider some
+   function Q : nat -> Set, and n : nat. In order to build a term of type Q n,
+   we can associate to the constructor O some term tO : Q O and to the pattern
+   'S p' some term tS : Q (S p). Note that the terms tO and tS do not have
+   the same type. The syntax of the dependent case analysis and its
+   associated typing rule are as follows:
+
+     Q : nat -> Set    tO : Q O    p : nat |- tp : Q (S p)    n : nat
+    ------------------------------------------------------------------
+     match n as nO return Q nO with | O => tO | S p => tS end : Q n
+
+   The former, non-dependent version of case analysis can be obtained from
+   this latter rule by taking Q as a constant function on n.
